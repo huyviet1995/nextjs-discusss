49f8b8ca612dc97e790b7e7320d247409500fc1b
vietdh <vietdanghuy.95@gmail.com>
2024-11-09 20:11:25 +0700

    Create auth to allow login via Github, display simple auth operations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 import { Button } from "@nextui-org/react";
-export default function Home() {
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button>Click me</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form> 
+      {
+        session?.user ? <div>Logged in as {session.user.email}</div> : <div>Signed out</div>
+      }
     </div>
   );
 }
