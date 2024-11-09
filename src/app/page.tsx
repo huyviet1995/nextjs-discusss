20d608814bb4362ac665b40877e731d98c3545b3
vietdh <vietdanghuy.95@gmail.com>
2024-11-09 20:35:08 +0700

    Create session providers to provide auth info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@nextui-org/react";
 import * as actions from "@/actions";
 import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
 export default async function Home() {
-  const session = await auth();
   return (
     <div>
       <form action={actions.signIn}>
@@ -13,9 +13,7 @@ export default async function Home() {
       <form action={actions.signOut}>
         <Button type="submit">Sign Out</Button>
       </form> 
-      {
-        session?.user ? <div>Logged in as {session.user.email}</div> : <div>Signed out</div>
-      }
+      <Profile />
     </div>
   );
 }
